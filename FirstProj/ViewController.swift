7b9c692e64c4151ee03c1d9644c1b738c5a88e95
nishantdhongadi <nishantdhongadi@gmail.com>
2023-07-27 21:31:22 +0800

    Updated button functionality to change background color.

--- FirstProj/ViewController.swift
+++ FirstProj/ViewController.swift
@@ -14,6 +14,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        
+        func changeColor() -> UIColor{
 
+            
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            
+        }
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+    }
+    
 }
 
